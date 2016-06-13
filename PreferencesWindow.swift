208e9c293cf9e221de039fd930eae2497e68348f
Li <me@wangjinli.com>
2016-06-13 15:23:21 +0800

    Preferences.

--- PreferencesWindow.swift
+++ PreferencesWindow.swift
@@ -6,10 +6,24 @@
 //  Copyright © 2016 Jinli Wang. All rights reserved.
 //
 
+protocol PreferencesWindowDelegate {
+    func preferencesDidUpdate()
+}
+
 import Cocoa
 
 class PreferencesWindow: NSWindowController, NSWindowDelegate {
 
+    @IBOutlet var startDatePicker: NSDatePicker!
+    @IBOutlet var lastCountField: NSTextField!
+    @IBAction func stepperClicked(sender: NSStepper) {
+        lastCountField.stringValue = "\(sender.intValue)"
+    }
+    @IBOutlet var displayFormatField: NSTextField!
+    @IBOutlet var autoLaunchButton: NSButton!
+    
+    var delegate: PreferencesWindowDelegate?
+    
     override var windowNibName: String! {
         return "PreferencesWindow"
     }
@@ -19,6 +33,24 @@ class PreferencesWindow: NSWindowController, NSWindowDelegate {
         self.window?.center()
         self.window?.makeKeyAndOrderFront(nil)
         NSApp.activateIgnoringOtherApps(true)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        startDatePicker.dateValue = defaults.valueForKey("startDate") as? NSDate ?? DEFAULT_STARTDATE
+        lastCountField.stringValue = defaults.stringForKey("lastCount") ?? String(DEFAULT_LASTCOUNT)
+        displayFormatField.stringValue = defaults.stringForKey("displayFormat") ?? DEFAULT_DISPLAYFORMAT
+        autoLaunchButton.state = defaults.valueForKey("autoLaunch") as? Int ?? DEFAULT_AUTOLAUNCH
+    }
+    
+    func windowWillClose(notification: NSNotification) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setValue(startDatePicker.dateValue, forKey: "startDate")
+        defaults.setValue(lastCountField.stringValue, forKey: "lastCount")
+        defaults.setValue(displayFormatField.stringValue, forKey: "displayFormat")
+        defaults.setValue(autoLaunchButton.state, forKey: "autoLaunch")
+        
+        defaults.synchronize()
+        
+        delegate?.preferencesDidUpdate()
     }
     
 }
--- WeekCount/StatusMenuController.swift
+++ WeekCount/StatusMenuController.swift
@@ -8,20 +8,31 @@
 
 import Cocoa
 
-class StatusMenuController: NSObject {
+let DEFAULT_STARTDATE = NSDate.init(timeIntervalSince1970: 1456099200)
+let DEFAULT_LASTCOUNT = 18
+let DEFAULT_DISPLAYFORMAT = "Week {W}"
+let DEFAULT_AUTOLAUNCH = NSOnState
+
+class StatusMenuController: NSObject, PreferencesWindowDelegate {
 
     @IBOutlet weak var statusMenu: NSMenu!
+    
     var preferencesWindow: PreferencesWindow!
+    var startDate: NSDate!
+    var lastCount: Int!
+    var displayFormat: String!
+    var autoLaunch: Int!
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     
     override func awakeFromNib() {
         
         preferencesWindow = PreferencesWindow()
+        preferencesWindow.delegate = self
         
-        if let button = statusItem.button {
-            button.title = "TEST"
-        }
+        updatePreferences()
+        
+        updateDisplay()
         
         statusMenu.itemArray.last!.action = Selector("terminate:")
         statusItem.menu = statusMenu
@@ -30,5 +41,24 @@ class StatusMenuController: NSObject {
     
     @IBAction func showPreferencesWindow(sender: NSMenuItem) {
         preferencesWindow.showWindow(nil)
+    }
+    
+    func preferencesDidUpdate() {
+        updatePreferences()
+        updateDisplay()
+    }
+    
+    func updatePreferences() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        startDate = defaults.valueForKey("startDate") as? NSDate ?? DEFAULT_STARTDATE
+        lastCount = Int(defaults.stringForKey("lastCount")!) ?? DEFAULT_LASTCOUNT
+        displayFormat = defaults.stringForKey("displayFormat") ?? DEFAULT_DISPLAYFORMAT
+        autoLaunch = defaults.valueForKey("autoLaunch") as? Int ?? DEFAULT_AUTOLAUNCH
+    }
+    
+    func updateDisplay() {
+        if let button = statusItem.button {
+            button.title = displayFormat
+        }
     }
 }
