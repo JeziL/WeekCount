cfdcc5a644a8bdc8c5a3785927dfd5e2a34d7746
Li <me@wangjinli.com>
2017-09-27 19:10:55 +0800

    fix year in copyright text

--- WeekCount/AboutWindow.swift
+++ WeekCount/AboutWindow.swift
@@ -11,6 +11,7 @@ import Cocoa
 class AboutWindow: NSWindowController, NSWindowDelegate {
 
     @IBOutlet var versionLabel: NSTextField!
+    @IBOutlet weak var copyrightLabel: NSTextField!
     
     @IBAction func gitHubButtonClicked(sender: NSButton) {
         NSWorkspace.shared.open(URL(string: "https://github.com/JeziL/WeekCount")!)
@@ -32,6 +33,10 @@ class AboutWindow: NSWindowController, NSWindowDelegate {
     
     func windowDidBecomeKey(_ notification: Notification) {
         versionLabel.stringValue = "v" + (Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        let currentYear = formatter.string(from: Date())
+        copyrightLabel.stringValue = "Copyright © 2016-\(currentYear) Jinli Wang.\nAll rights reserved."
     }
     
     
